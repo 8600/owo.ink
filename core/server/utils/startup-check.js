257e9e30cf99fa4bba2bb08a22458cb541be55d7
PUGE <my@owo.help>
2016-12-31 20:58:17 +0800

    自动检测

--- core/server/utils/startup-check.js
+++ core/server/utils/startup-check.js
@@ -1,12 +1,10 @@
-var packages = require('../../../package.json'),
-    path = require('path'),
-    crypto = require('crypto'),
-    fs = require('fs'),
-    mode = process.env.NODE_ENV === undefined ? 'development' : process.env.NODE_ENV,
-    appRoot = path.resolve(__dirname, '../../../'),
-    configFilePath = process.env.GHOST_CONFIG || path.join(appRoot, 'config.js'),
-    checks,
-    exitCodes = {
+const packages = require('../../../package.json'),
+      path = require('path'),
+      crypto = require('crypto'),
+      fs = require('fs'),
+      mode = process.env.NODE_ENV === undefined ? 'development' : process.env.NODE_ENV,
+      appRoot = path.resolve(__dirname, '../../../');
+let   exitCodes = {
         NODE_VERSION_UNSUPPORTED: 231,
         NODE_ENV_CONFIG_MISSING: 232,
         DEPENDENCIES_MISSING: 233,
@@ -14,7 +12,9 @@ var packages = require('../../../package.json'),
         CONTENT_PATH_NOT_WRITABLE: 235,
         SQLITE_DB_NOT_WRITABLE: 236,
         BUILT_FILES_DO_NOT_EXIST: 237
-    };
+    },
+    configFilePath = process.env.GHOST_CONFIG || path.join(appRoot, 'config.js'),
+    checks;
 
 checks = {
     check: function check() {
@@ -27,56 +27,46 @@ checks = {
         this.builtFilesExist();
     },
 
-    // Make sure the node version is supported
+    // 确保node版本正确
     nodeVersion: function checkNodeVersion() {
-        // Tell users if their node version is not supported, and exit
-        var semver = require('semver');
-
+        // 如果使用了不支持的node版本，退出程序
+        const semver = require('semver');
         if (process.env.GHOST_NODE_VERSION_CHECK !== 'false' &&
             !semver.satisfies(process.versions.node, packages.engines.node)) {
-            console.error('\x1B[31mERROR: Unsupported version of Node');
-            console.error('\x1B[31mGhost needs Node version ' + packages.engines.node +
-                          ' you are using version ' + process.versions.node + '\033[0m\n');
-            console.error('\x1B[32mPlease see http://support.ghost.org/supported-node-versions/ for more information\033[0m');
-
+            console.error('\x1B[31m错误: 不支持的node版本');
+            console.error('\x1B[31m博客建议Node版本:' + packages.engines.node +
+                          '\n本机安装Node版本:' + process.versions.node + '\033[0m\n');
             process.exit(exitCodes.NODE_VERSION_UNSUPPORTED);
         }
+        console.log("√ Node版本符合要求");
     },
 
     nodeEnv: function checkNodeEnvState() {
-        // Check if config path resolves, if not check for NODE_ENV in config.example.js prior to copy
-        var fd,
-            configFile,
-            config;
-
+        // 检查配置文件是否存在
+        let fd,configFile,config;
         try {
             fd = fs.openSync(configFilePath, 'r');
             fs.closeSync(fd);
         } catch (e) {
+            //如果不存在将config.example作为配置文件
             configFilePath = path.join(appRoot, 'config.example.js');
         }
-
         configFile = require(configFilePath);
         config = configFile[mode];
-
         if (!config) {
-            console.error('\x1B[31m错误: 找不到配置项NODE_ENV: ' +
-                            process.env.NODE_ENV + '\033[0m\n');
-            console.error('\x1B[32m请确认你的config.js配置了NODE_ENV' +
-                            ' 并且格式正确.\033[0m');
-
+            console.error('\x1B[31m错误: 找不到配置项NODE_ENV: ' +process.env.NODE_ENV + '\033[0m\n');
+            console.error('\x1B[32m请确认你的config.js配置了NODE_ENV' +' 并且格式正确.\033[0m');
             process.exit(exitCodes.NODE_ENV_CONFIG_MISSING);
         }
+        console.log("√ 配置文件没有错误");
     },
 
-    // Make sure package.json dependencies have been installed.
+    // 确保package.json依赖已经完全安装.
     packages: function checkPackages() {
         if (mode !== 'production' && mode !== 'development') {
             return;
         }
-
-        var errors = [];
-
+        let errors = [];
         Object.keys(packages.dependencies).forEach(function (p) {
             try {
                 require.resolve(p);
@@ -88,9 +78,7 @@ checks = {
         if (!errors.length) {
             return;
         }
-
         errors = errors.join('\n  ');
-
         console.error('\x1B[31m错误: 丢失关键性组件:\033[0m\n  ' + errors);
         console.error('\x1B[32m\n请运行`npm install --production`安装组件后尝试再次运行.');
         console.error('\x1B[32m或者查看帮助文档 http://support.ghost.org.\033[0m\n');
@@ -98,60 +86,56 @@ checks = {
         process.exit(exitCodes.DEPENDENCIES_MISSING);
     },
 
-    // Check content path permissions
+    // 检查content文件夹权限
     contentPath: function checkContentPaths() {
         if (mode !== 'production' && mode !== 'development') {
             return;
         }
-
-        var configFile,
+        let configFile,
             config,
             contentPath,
             contentSubPaths = ['apps', 'data', 'images', 'themes'],
             fd,
-            errorHeader = '\x1B[31mERROR: Unable to access Ghost\'s content path:\033[0m',
-            errorHelp = '\x1B[32mCheck that the content path exists and file system permissions are correct.' +
-                '\nHelp and documentation can be found at http://support.ghost.org.\033[0m';
+            errorHeader = '\x1B[31m× access目录权限异常:\033[0m';
 
-        // Get the content path to test.  If it's defined in config.js use that, if not use the default
+        // 获取content文件夹路径.  如果config.js中有定义，则使用config.js中的路径
         try {
             configFile = require(configFilePath);
             config = configFile[mode];
-
             if (config && config.paths && config.paths.contentPath) {
                 contentPath = config.paths.contentPath;
             } else {
                 contentPath = path.join(appRoot, 'content');
             }
         } catch (e) {
-            // If config.js doesn't exist yet, check the default content path location
+            // 如果config.js没有定义, 使用默认路径
             contentPath = path.join(appRoot, 'content');
         }
 
-        // Use all sync io calls so that we stay in this function until all checks are complete
-
-        // Check the root content path
         try {
             fd = fs.openSync(contentPath, 'r');
             fs.closeSync(fd);
         } catch (e) {
             console.error(errorHeader);
             console.error('  ' + e.message);
-            console.error('\n' + errorHelp);
 
             process.exit(exitCodes.CONTENT_PATH_NOT_ACCESSIBLE);
         }
 
-        // Check each of the content path subdirectories
+        // 遍历检查content子目录
         try {
             contentSubPaths.forEach(function (sub) {
-                var dir = path.join(contentPath, sub),
-                    randomFile = path.join(dir, crypto.randomBytes(8).toString('hex'));
-
+                const dir = path.join(contentPath, sub);
+                if(!fs.existsSync(dir)){
+                    console.log(`! ${dir}目录不存在`);
+                    console.log(`√ ${dir}目录已创建`);
+                    fs.mkdirSync(dir);
+                }
+                const    randomFile = path.join(dir, crypto.randomBytes(8).toString('hex'));
                 fd = fs.openSync(dir, 'r');
                 fs.closeSync(fd);
 
-                // Check write access to directory by attempting to create a random file
+                // 检查写权限，写一个随机文件
                 fd = fs.openSync(randomFile, 'wx+');
                 fs.closeSync(fd);
                 fs.unlinkSync(randomFile);
@@ -159,19 +143,18 @@ checks = {
         } catch (e) {
             console.error(errorHeader);
             console.error('  ' + e.message);
-            console.error('\n' + errorHelp);
-
             process.exit(exitCodes.CONTENT_PATH_NOT_WRITABLE);
         }
+        console.log("√ content目录权限正常");
     },
 
-    // Make sure sqlite3 database is available for read/write
+    // 确保sqlite3数据库可以读写
     sqlite: function checkSqlite() {
         if (mode !== 'production' && mode !== 'development') {
             return;
         }
 
-        var configFile,
+        let configFile,
             config,
             appRoot = path.resolve(__dirname, '../../../'),
             dbPath,
@@ -181,7 +164,7 @@ checks = {
             configFile = require(configFilePath);
             config = configFile[mode];
 
-            // Abort check if database type is not sqlite3
+            // 检查数据库文件是否为sqlite3
             if (config && config.database && config.database.client !== 'sqlite3') {
                 return;
             }
@@ -190,27 +173,27 @@ checks = {
                 dbPath = config.database.connection.filename;
             }
         } catch (e) {
-            // If config.js doesn't exist, use the default path
+            //如果config.js没有配置, 使用默认目录
             dbPath = path.join(appRoot, 'content', 'data', mode === 'production' ? 'ghost.db' : 'ghost-dev.db');
         }
 
-        // Check for read/write access on sqlite db file
+        // 检查数据库读／写权限
         try {
             fd = fs.openSync(dbPath, 'r+');
             fs.closeSync(fd);
         } catch (e) {
-            // Database file not existing is not an error as sqlite will create it.
+            // 数据库文件如果不存在，sqlite3会创建一个
             if (e.code === 'ENOENT') {
+                console.log("! 数据库文件不存在");
+                console.log("√ 数据库文件以创建");
                 return;
             }
 
-            console.error('\x1B[31mERROR: Unable to open sqlite3 database file for read/write\033[0m');
+            console.error('\x1B[31m错误: 无法正常读写数据库\033[0m');
             console.error('  ' + e.message);
-            console.error('\n\x1B[32mCheck that the sqlite3 database file permissions allow read and write access.');
-            console.error('Help and documentation can be found at http://support.ghost.org.\033[0m');
-
             process.exit(exitCodes.SQLITE_DB_NOT_WRITABLE);
         }
+        console.log("√ 数据库文件正常");
     },
 
     mail: function checkMail() {
@@ -225,9 +208,10 @@ checks = {
         }
 
         if (!config.mail || !config.mail.transport) {
-            console.error('\x1B[31m警告: 你还没有为博客系统配置邮件系统. \n建议你配置一个邮件系统.\033[0m');
+            console.log("! 邮件系统未配置");
             console.error('\x1B[32m你可以查阅帮助文档 http://support.ghost.org/mail.\033[0m\n');
         }
+        console.log("√ 邮件系统已配置");
     },
 
     builtFilesExist: function builtFilesExist() {
