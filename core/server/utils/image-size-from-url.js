e1445ec5e5c2ef0f5b169e9d539622672ce67f51
PUGE <mmit7750>
2017-02-27 23:45:00 +0800

    save

--- core/server/utils/image-size-from-url.js
+++ core/server/utils/image-size-from-url.js
@@ -1,62 +1,56 @@
-// Supported formats of https://github.com/image-size/image-size:
+// https://github.com/image-size/image-size:
+// 支持的格式
 // BMP, GIF, JPEG, PNG, PSD, TIFF, WebP, SVG
 // ***
 // Takes the url of the image and an optional timeout
-// getImageSizeFromUrl returns an Object like this
+// 返回格式
 // {
 //     height: 50,
 //     url: 'http://myblog.com/images/cat.jpg',
 //     width: 50
 // };
-// if the dimensions can be fetched and rejects with error, if not.
-// ***
-// In case we get a locally stored image or a not complete url (like //www.gravatar.com/andsoon),
-// we add the protocol to the incomplete one and use urlFor() to get the absolute URL.
-// If the request fails or image-size is not able to read the file, we reject with error.
 
-var sizeOf       = require('image-size'),
-    url          = require('url'),
-    Promise      = require('bluebird'),
-    http         = require('http'),
-    https        = require('https'),
-    config       = require('../config'),
-    dimensions,
-    request,
-    requestHandler;
+const sizeOf       = require('image-size'),
+      url          = require('url'),
+      Promise      = require('bluebird'),
+      http         = require('http'),
+      https        = require('https'),
+      config       = require('../config'),
+      dimensions,
+      request,
+      requestHandler;
 
 /**
- * @description read image dimensions from URL
+ * @description 从URL读取图像尺寸
  * @param {String} imagePath
  * @param {Number} timeout (optional)
  * @returns {Promise<Object>} imageObject or error
  */
 module.exports.getImageSizeFromUrl = function getImageSizeFromUrl(imagePath, timeout) {
     return new Promise(function imageSizeRequest(resolve, reject) {
-        var imageObject = {},
-            options;
-
+        let imageObject = {},options;
         imageObject.url = imagePath;
 
-        // check if we got an url without any protocol
+        // 纠正不正确的URL格式
         if (imagePath.indexOf('http') === -1) {
             // our gravatar urls start with '//' in that case add 'http:'
             if (imagePath.indexOf('//') === 0) {
                 // it's a gravatar url
                 imagePath = 'http:' + imagePath;
             } else {
-                // get absolute url for image
+                // 获取图像的绝对url
                 imagePath = config.urlFor('image', {image: imagePath}, true);
             }
         }
 
         options = url.parse(imagePath);
-
+        //判断是否为HTTPS
         requestHandler = imagePath.indexOf('https') === 0 ? https : http;
         options.headers = {'User-Agent': 'Mozilla/5.0'};
 
         request = requestHandler.get(options, function (res) {
-            var chunks = [];
-
+            //存储数据
+            let chunks = [];
             res.on('data', function (chunk) {
                 chunks.push(chunk);
             });
@@ -71,20 +65,18 @@ module.exports.getImageSizeFromUrl = function getImageSizeFromUrl(imagePath, tim
 
                         return resolve(imageObject);
                     } catch (err) {
-                        // @ToDo: add real error handling here as soon as we have error logging
+                        // 如果出错返回错误信息
                         return reject(err);
                     }
                 } else {
-                    // @ToDo: add real error handling here as soon as we have error logging
-                    var err = new Error();
+                    let err = new Error();
                     err.message = imagePath;
                     err.statusCode = res.statusCode;
-
                     return reject(err);
                 }
             });
         }).on('socket', function (socket) {
-            // don't set timeout if no timeout give as argument
+            // 如果没有超时作为参数，则不设置超时
             if (timeout) {
                 socket.setTimeout(timeout);
                 socket.on('timeout', function () {
@@ -92,8 +84,6 @@ module.exports.getImageSizeFromUrl = function getImageSizeFromUrl(imagePath, tim
                 });
             }
         }).on('error', function (err) {
-            // @ToDo: add real error handling here as soon as we have error logging
-
             return reject(err);
         });
     });
